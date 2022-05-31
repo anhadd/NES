0d8e5b15789a0b0ba291731d3bf1d726c66e428b
anhadd <78433229+anhadd@users.noreply.github.com>
2022-05-31 20:14:58 +0200

    Added else and changed condition.

--- src/Mapper0.cpp
+++ src/Mapper0.cpp
@@ -19,13 +19,18 @@ uint32_t Mapper0::cpuMap(uint16_t address, bool write, uint8_t value) {
     if (address >= 0x6000 && address <= 0x7FFF) {
         return address & 0x1FFF;
     }
-    else {
+    else if (address >= 0x8000 && address <= 0xFFFF) {
         if (PRG_banks == 1) {
             return address & 0x3FFF;
         }
         else {
             return address & 0x7FFF;
         }
+    }
+    else {
+        // Should never reach this.
+        fprintf(stderr, "Error: Access not allowed on Mapper0.\n");
+        exit(0);
     }
 }
 
