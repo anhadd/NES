77e6ec930ec0f9c82dc494873e9f83870d35425f
anhadd <78433229+anhadd@users.noreply.github.com>
2022-06-01 15:52:08 +0200

    Added missing memory areas. Added debug log boolean.

--- src/BUS.cpp
+++ src/BUS.cpp
@@ -13,6 +13,8 @@ BUS::BUS() {
     oam_index = 0x00;
     oam_writing = false;
     cpu_synchronized = false;
+
+    debug_log = false;
 }
 
 BUS::~BUS() {
@@ -44,19 +46,31 @@ void BUS::passROM(ROM* nesROM) {
 // CPU reading from the BUS.
 uint8_t BUS::busReadCPU(uint16_t address) {
     if (address >= 0x0000 && address <= 0x1FFF) {
+        // if (debug_log) {
+        //     fprintf(stderr, "RAM READ : %04x        %02x\n", address & 0x07FF, memory[address & 0x07FF]);
+        // }
         return memory[address & 0x07FF];
     }
     else if (address >= 0x2000 && address <= 0x3FFF) {
         return ppu->readRegister(address);
+    }
+    else if (address >= 0x4000 && address <= 0x4015) {
+        // TODO: These registers are not yet implemented.
     }
     else if (address == 0x4016 || address == 0x4017) {
         uint8_t temp = controller_shift[address % 0x4016];
         controller_shift[address % 0x4016] <<= 1;
         return (temp & 0x80) != 0;
     }
+    else if(address >= 0x4018 && address <= 0x401F) {
+        // TODO: APU and I/O functionality that is normally disabled.
+    }
+    else if (address >= 0x4020 && address <= 0x5FFF) {
+        // TODO: Expansion ROM.
+        return rom->Expansion_ROM[address % 0x4020];
+    }
     else if (address >= 0x6000 && address <= 0x7FFF) {
         // TODO: CHECK WHY SOME MAPPER 2 ROMS USE THIS (THOUGHT THEY SHOULDNT ?)
-        // fprintf(stderr, "!!!!! PRG RAM USED !!!!!!\n");
         return rom->PRG_ram[rom->mapper->cpuMap(address, false)];
     }
     else if (address >= 0x8000 && address <= 0xFFFF) {
@@ -68,6 +82,9 @@ uint8_t BUS::busReadCPU(uint16_t address) {
 // CPU writing to the BUS.
 uint8_t BUS::busWriteCPU(uint16_t address, uint8_t value) {
     if (address >= 0x0000 && address <= 0x1FFF) {
+        // if (debug_log) {
+        //     fprintf(stderr, "RAM WRITE: %04x        %02x\n", address & 0x07FF, value);
+        // }
         memory[address & 0x07FF] = value;
     }
     else if (address >= 0x2000 && address <= 0x3FFF) {
@@ -78,17 +95,26 @@ uint8_t BUS::busWriteCPU(uint16_t address, uint8_t value) {
         oam_index = 0x00;
         oam_writing = true;
     }
+    else if (address >= 0x4000 && address <= 0x4015 && address != 0x4014) {
+        // TODO: These registers are not yet implemented.
+    }
     else if (address == 0x4016 || address == 0x4017) {
         controller_shift[address % 0x4016] = controller[address % 0x4016];
     }
+    else if(address >= 0x4018 && address <= 0x401F) {
+        // TODO: APU and I/O functionality that is normally disabled.
+    }
+    else if (address >= 0x4020 && address <= 0x5FFF) {
+        // TODO: Expansion ROM.
+        rom->Expansion_ROM[address % 0x4020] = value;
+    }
     else if (address >= 0x6000 && address <= 0x7FFF) {
         // TODO: CHECK WHY SOME MAPPER 2 ROMS USE THIS (THOUGHT THEY SHOULDNT ?)
-        // fprintf(stderr, "!!!!! PRG RAM USED !!!!!!\n");
         rom->PRG_ram[rom->mapper->cpuMap(address, true, value)] = value;
     }
     else if (address >= 0x8000 && address <= 0xFFFF) {
-        // Writing not allowed for PRG memory.
-        rom->mapper->cpuMap(address, true, value);
+        // TODO: Check if writing is not allowed for PRG memory.
+        rom->PRG_memory[rom->mapper->cpuMap(address, true, value)] = value;
     }
     return 0;
 }
